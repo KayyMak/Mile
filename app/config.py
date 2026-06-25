8fb84fb19a1496a53d5d38f2b7f244057955b920
KayMakk <bomo794@gmail.com>
2026-06-24 22:04:42 -0700

    more stuff

--- app/config.py
+++ app/config.py
@@ -8,4 +8,5 @@ db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME") 
+secret_key=os.getenv("SECRET_KEY")
 
--- app/schemas.py
+++ app/schemas.py
@@ -12,6 +12,10 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
 class UserResponse(BaseModel):
     id: int
     email: str
