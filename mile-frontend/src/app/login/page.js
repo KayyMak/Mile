8826caa662518d45632d3b3beadd1eb204e4e2f0
KayMakk <bomo794@gmail.com>
2026-09-03 22:59:54 -0700

    prog

--- mile-frontend/src/app/login/page.js
+++ mile-frontend/src/app/login/page.js
@@ -1,9 +1,15 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
   return (
     <div>
-      {/* TODO: login form */}
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
     </div>
   );
 }
